1eeca1e9049d2e63a2e1bea6db4993fa14668bc1
Ruairidh Williamson <williamsonruairidh@gmail.com>
2024-01-27 09:52:50 +0000

    Added options page

---
+++ options/options.js
@@ -0,0 +1,77 @@
+const defaultActiveList = [
+  "wordle",
+];
+
+browser.storage.sync.get("active_list").then(activeList => {
+  activeList = activeList.active_list || defaultActiveList;
+  const activeListElem = document.getElementById("active-list");
+  const inactiveListElem = document.getElementById("inactive-list");
+
+  let draggingItem = null;
+
+  const dragUpdate = (list, ev) => {
+    ev.preventDefault();
+    for (const child of list.children) {
+      if (ev.clientY <= child.offsetTop + child.offsetHeight / 2) {
+        if (child === draggingItem) return;
+        child.before(draggingItem);
+        return;
+      }
+    }
+    list.append(draggingItem);
+  };
+
+  [activeListElem, inactiveListElem].forEach(list => {
+    list.addEventListener("dragover", ev => dragUpdate(list, ev));
+    list.addEventListener("dragenter", ev => dragUpdate(list, ev));
+  });
+
+  const createRow = (game) => {
+    const row = document.createElement("div");
+    row.classList.add("game-row");
+    row.draggable = true;
+    row.setAttribute("gameid", game.id);
+    row.addEventListener("dragstart", ev => {
+      ev.dataTransfer.setData("text/uri-list", game.url);
+      ev.dataTransfer.setData("text/plain", game.url);
+      ev.target.classList.add("dragging");
+      draggingItem = row;
+    });
+    row.addEventListener("dragend", ev => {
+      ev.target.classList.remove("dragging");
+      draggingItem = null;
+      let newActiveList = [];
+      for (const child of activeListElem.children) {
+        const game_id = child.getAttribute("gameid");
+        newActiveList.push(game_id);
+      }
+      browser.storage.sync.set({
+        "active_list": newActiveList,
+      });
+    });
+    const icon = document.createElement("img");
+    icon.src = "https://s2.googleusercontent.com/s2/favicons?domain_url=" + game.url;
+    const label = document.createElement("a");
+    label.innerText = game.label;
+    row.append(icon, label);
+    return row;
+  };
+
+  activeList.forEach(game_id => {
+    const game = GAMES.find(g => g.id === game_id);
+    if (game === undefined) {
+      return;
+    }
+    activeListElem.append(createRow(game));
+  });
+  GAMES.forEach(game => {
+    if (!activeList.includes(game.id)) {
+      inactiveListElem.append(createRow(game));
+    }
+  });
+});
+
+const openAllBtn = document.getElementById("open-all");
+openAllBtn.addEventListener("click", async () => {
+  await openActive();
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,34 +1,11 @@
-const isFirefox = typeof browser !== "undefined";
-const isChrome = !isFirefox;
 
 const openAllBtn = document.getElementById("open-all");
 const settingsBtn = document.getElementById("settings");
 
-openAllBtn.addEventListener("click", () => {
-  const gameUrls = [
-    // "http://syndication.andrewsmcmeel.com/puzzles/crosswords",
-    // "crossword/index.html",
-    "https://www.theglobeandmail.com/puzzles-and-crosswords/universal-crossword/",
+openAllBtn.addEventListener("click", async () => {
+  await openActive();
+});
 
-    "https://www.nytimes.com/games/wordle/index.html",
-    "https://worldle.teuteuf.fr",
-    "https://travle.earth/",
-    "https://wheretaken.teuteuf.fr",
-    "https://wheretakenusa.teuteuf.fr",
-    "https://framed.wtf",
-    "https://episode.wtf",
-    "https://semantle.pimanrul.es",
-  ];
-  const createOptions = {
-    active: false,
-  };
-  if (isFirefox) {
-    createOptions.discarded = true;
-  }
-  gameUrls.forEach(url => {
-    chrome.tabs.create({
-      url,
-      ...createOptions
-    });
-  })
+settingsBtn.addEventListener("click", () => {
+  chrome.runtime.openOptionsPage();
 });
